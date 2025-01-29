d7f13a09d951a6fd19313bbdec3593c67c9af317
Vimukthi Shehan <151540426+vimukthi77@users.noreply.github.com>
2025-01-29 14:55:45 +0530

    5th commit

---
+++ app/src/main/java/lk/sliita/notebook/Note.java
@@ -0,0 +1,37 @@
+package lk.sliita.notebook;
+
+import com.google.firebase.Timestamp;
+
+public class Note {
+    String title;
+    String content;
+    Timestamp timestamp;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Note() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+}
